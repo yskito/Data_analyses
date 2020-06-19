d61f4ef5c47553f6036376cc6f8ef3411db63aa5
Yusuke Ito <40048300+yskito@users.noreply.github.com>
2020-06-19 12:15:04 +0900

    Add files via upload

---
+++ R_data_visualization.R
@@ -0,0 +1,41 @@
+#========================================
+# Setting
+#========================================
+rm(list=ls(all=TRUE))
+dirname(rstudioapi::getSourceEditorContext()$path)
+
+# Load required packages.
+library(psych)
+library(GGally)
+library(ggplot2)
+
+#========================================
+# visualize data 
+#========================================
+pdf_name = "data_visualization.pdf"
+data = iris
+
+pdf(pdf_name, height=10, width=10)
+
+# 1. pairs
+pairs.panels(data, hist.col="#99CCFF")#, bg = as.numeric(iris$Species), pch = 21)
+
+# 2. histgram (正確な分布の確認)
+if( length(data) %%2 == 0){ ncol=(length(data)/2)}
+if( length(data) %%2 == 1){ ncol=(length(data)/2)+1}
+par( mfrow=c(2,ncol ) )
+for (i in 1:length(data)) {
+   if( is.numeric(data[,i]) == TRUE ){
+      hist(data[,i], freq=FALSE, col="#99CCFF", border="#0066FF", main = paste(names(data)[i], sep=""), xlab="",cex.main=1.5, cex.lab=1, cex.axis=1, font.lab=2, font.axis=2 )
+   }
+}
+# 3. ggpairs (グループ分けした際の正確な分布の確認)
+ggpairs( data ) + theme_bw();
+
+dev.off()
+
+print( summary( data ) )
+   
+
+
+
